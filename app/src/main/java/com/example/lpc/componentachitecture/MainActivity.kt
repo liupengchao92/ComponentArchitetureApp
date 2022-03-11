b901aa415dabffabec30661039c8b6e72ecbc35e
LPC <876367352@qq.com>
2022-03-11 10:31:15 +0800

    提交代码

--- app/src/main/java/com/example/lpc/componentachitecture/MainActivity.kt
+++ app/src/main/java/com/example/lpc/componentachitecture/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.lpc.componentachitecture
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lpc.library.TestUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val string = TestUtils.getString();
+
+        Log.e("MainActivity", "onCreate: ${string}", )
+
+
+
     }
 }
