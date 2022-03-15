43f6ada91a62feeabc45e2107b9e164b1f08d1ab
LPC <876367352@qq.com>
2022-03-15 17:08:33 +0800

    删除.Idea文件

---
+++ app/src/main/java/com/example/lpc/componentachitecture/splash/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.lpc.componentachitecture.splash
+
+import android.os.Bundle
+import com.example.lpc.componentachitecture.R
+import com.example.lpc.lib_common.base.BaseActivity
+
+class SplashActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
