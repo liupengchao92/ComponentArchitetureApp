ecf25d4a4664db7c264f4cf174f8361e39af8efd
LPC <876367352@qq.com>
2022-03-23 14:44:47 +0800

    创建启动Model

---
+++ start_init/src/main/java/com/example/lpc/start_init/api/StartUpManager.kt
@@ -0,0 +1,14 @@
+package com.example.lpc.start_init.api
+
+/**
+ * Author: liupengchao
+ * Date: 2022/3/23
+ * ClassName :StartUpManager
+ * Desc:启动初始化类
+ */
+class StartUpManager {
+
+    fun init() {
+
+    }
+}
---
+++ start_init/src/main/java/com/example/lpc/start_init/api/TestProcessor.kt
@@ -0,0 +1,12 @@
+package com.example.lpc.start_init.api
+
+/**
+ * Author: liupengchao
+ * Date: 2022/3/23
+ * ClassName :TestProcessor
+ * Desc:
+ */
+class TestProcessor{
+
+
+}
