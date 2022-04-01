e1c711251cf3657cc9367bad39a73cbb4a334643
LPC <876367352@qq.com>
2022-04-01 15:09:27 +0800

    开屏页动画

--- app/src/main/java/com/example/lpc/componentachitecture/splash/SplashActivity.kt
+++ app/src/main/java/com/example/lpc/componentachitecture/splash/SplashActivity.kt
@@ -1,6 +1,10 @@
 package com.example.lpc.componentachitecture.splash
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.animation.DecelerateInterpolator
 import androidx.activity.viewModels
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
@@ -22,29 +26,61 @@ class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val animator = AnimatorSet()
+        val scaleX = ObjectAnimator.ofFloat(welcome, "scaleX", 1f, 2f, 1f);
+        val scaleY = ObjectAnimator.ofFloat(welcome, "scaleY", 1f, 2f, 1f);
+        animator.duration = 1500;
+        animator.interpolator = DecelerateInterpolator()
+        animator.play(scaleX).with(scaleY);
+        animator.start()
+        animator.addonAnimationEnd {
+            goToMain()
+        }
+    }
 
-        gotoMain.setOnClickListener {
-            //跳转到主Activity
+    private fun goToMain() {
+        //跳转到主Activity
 
-            var userName: String? = MMKVUtils.getString(MMKVConstant.USER_NAME)
-            var password: String? = MMKVUtils.getString(MMKVConstant.PASSWORD)
+        var userName: String? = MMKVUtils.getString(MMKVConstant.USER_NAME)
+        var password: String? = MMKVUtils.getString(MMKVConstant.PASSWORD)
 
-            if (!userName.isNullOrBlank() && !password.isNullOrBlank()) {
-                loginViewModel.login(userName, password)
+        if (!userName.isNullOrBlank() && !password.isNullOrBlank()) {
+            loginViewModel.login(userName, password)
+        } else {
+            //跳转到登录页面
+            ARouter.getInstance().build(ARouterConstant.Main.LOGIN_PATH).navigation()
+        }
+
+        loginViewModel.loginResult.observe(this) {
+
+            if (it.success != null) {
+                ARouter.getInstance().build(ARouterConstant.Main.INDEX_PATH).navigation()
             } else {
                 //跳转到登录页面
                 ARouter.getInstance().build(ARouterConstant.Main.LOGIN_PATH).navigation()
             }
 
-            loginViewModel.loginResult.observe(this) {
-
-                if (it.success != null) {
-                    ARouter.getInstance().build(ARouterConstant.Main.INDEX_PATH).navigation()
-                } else {
-                    //跳转到登录页面
-                    ARouter.getInstance().build(ARouterConstant.Main.LOGIN_PATH).navigation()
-                }
-            }
+            finish()
         }
     }
+}
+
+fun Animator.addonAnimationEnd(onAnimationEnd: (animation: Animator?) -> Unit) {
+
+    this.addListener(object : Animator.AnimatorListener {
+
+        override fun onAnimationStart(animation: Animator?) {
+
+        }
+
+        override fun onAnimationEnd(animation: Animator?) {
+            onAnimationEnd.invoke(animation)
+        }
+
+        override fun onAnimationCancel(animation: Animator?) {
+        }
+
+        override fun onAnimationRepeat(animation: Animator?) {
+        }
+    })
 }
