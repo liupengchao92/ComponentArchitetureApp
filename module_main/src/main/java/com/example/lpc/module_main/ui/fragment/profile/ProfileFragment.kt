a57b31e3c852a5c965ef4a55edd4a0b650cb2338
LPC <876367352@qq.com>
2022-05-18 11:49:45 +0800

    创建个人模块的moddule

--- module_main/src/main/java/com/example/lpc/module_main/ui/fragment/profile/ProfileFragment.kt
+++ module_main/src/main/java/com/example/lpc/module_main/ui/fragment/profile/ProfileFragment.kt
@@ -1,19 +1,12 @@
 package com.example.lpc.module_main.ui.fragment.profile
 
-import android.app.StatusBarManager
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.blankj.utilcode.util.BarUtils
-import com.blankj.utilcode.util.ColorUtils
-import com.blankj.utilcode.util.SizeUtils
 import com.blankj.utilcode.util.ToastUtils
 import com.example.lpc.lib_common.base.fragment.BaseFragment
 import com.example.lpc.module_main.R
-import com.example.lpc.module_main.ui.fragment.profile.ProfileDataSource
-import com.example.lpc.module_main.ui.fragment.profile.ProfileRepository
-import com.example.lpc.module_main.ui.fragment.profile.ProfileViewModel
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 /**
@@ -22,7 +15,7 @@ import kotlinx.android.synthetic.main.fragment_profile.*
  * ClassName :ProfileFragment
  * Desc:个人
  */
-class ProfileFragment : BaseFragment(),View.OnClickListener {
+class ProfileFragment : BaseFragment(), View.OnClickListener {
 
     private val viewModel: ProfileViewModel by viewModels {
         object : ViewModelProvider.Factory {
@@ -37,10 +30,9 @@ class ProfileFragment : BaseFragment(),View.OnClickListener {
 
     override fun onCreate() {
         viewModel.userInfoData.observe(this) {
-
             userNameTv.text = it.userInfo.username
         }
-
+        messageLayout.setOnClickListener(this)
         integralLayout.setOnClickListener(this)
         favoriteLayout.setOnClickListener(this)
         shareLayout.setOnClickListener(this)
@@ -54,18 +46,20 @@ class ProfileFragment : BaseFragment(),View.OnClickListener {
 
     override fun onClick(v: View?) {
 
-        when (v?.id){
-
-            R.id.integralLayout ->{
+        when (v?.id) {
+            R.id.messageLayout -> {
+                ToastUtils.showShort(R.string.my_message)
+            }
+            R.id.integralLayout -> {
                 ToastUtils.showShort(R.string.my_integral)
             }
-            R.id.favoriteLayout ->{
+            R.id.favoriteLayout -> {
                 ToastUtils.showShort(R.string.my_favorite)
             }
-            R.id.shareLayout ->{
+            R.id.shareLayout -> {
                 ToastUtils.showShort(R.string.my_share)
             }
-            R.id.settingLayout ->{
+            R.id.settingLayout -> {
                 ToastUtils.showShort(R.string.my_setting)
             }
         }
